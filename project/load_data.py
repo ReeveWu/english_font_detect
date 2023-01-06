ab2f056e4f92063f51140f088b4954aff03d2516
reeve-zc <110542858+reeve-zc@users.noreply.github.com>
2023-01-06 14:42:11 +0800

    Update load_data.py

--- project/load_data.py
+++ project/load_data.py
@@ -4,9 +4,11 @@ import time
 import numpy as np
 from dictionary import *
 
+# 將資料和標籤轉換為numpy陣列
 def data_append(file, file_name, X_train, Y_train, kind, type='one'):
     start = time.time()
     X_tmp, Y_tmp = [], []
+    # 讀取圖片
     for index, data in enumerate(file_name):
         l = data.split('b)')[-1].split('-')[0] if '(b)' in data else data.split('-')[0]
         if type == 'all':
@@ -36,7 +38,8 @@ def data_append(file, file_name, X_train, Y_train, kind, type='one'):
 
         print(
             f'\rLoading data : {l} - {math.ceil((index + 1) * 100 / len(file_name))}% (data:{(index + 1)}/{len(file_name)}) -{round((time.time()-start), 2)}s', end='')
-
+    
+    # 轉換/合併為numpy陣列
     if len(X_train) != 0:
         X = np.concatenate([X_train, X_tmp])
         Y = np.concatenate([Y_train, Y_tmp])
@@ -46,6 +49,7 @@ def data_append(file, file_name, X_train, Y_train, kind, type='one'):
 
     return X, Y
 
+# 讀取單一字母資料(用於字體辨識)
 def letter(letter, type):
     X, Y = [], []
     data_file = os.listdir('dataset/{0}'.format(type))
@@ -56,7 +60,7 @@ def letter(letter, type):
             X, Y = data_append(file, datas, X, Y, type)
 
     return X, Y
-
+# 讀取整體資料(用於字母辨識)
 def all(type):
     all_fonts = []
     X, Y = [], []
@@ -74,14 +78,17 @@ def all(type):
 
     return X, Y
 
+# 打亂資料
 def data_shuffle(X, Y):
     shuffle_ix = np.random.permutation(np.arange(len(Y)))
     X = np.array(X)[shuffle_ix]
     Y = np.array(Y)[shuffle_ix]
     return X, Y
 
+# 呼叫此函式載入目標資料集
 def load_data(*args):
-    if args == ():
+    if args == ():  #載入整體資料集
+        # 嘗試讀取以儲存的.npy檔
         try:
             X_train = np.load('dataset_array/All_X_train.npy')
             Y_train = np.load('dataset_array/All_Y_train.npy')
@@ -89,6 +96,7 @@ def load_data(*args):
             Y_test = np.load('dataset_array/All_Y_test.npy')
             print('Successful loading of data : All')
         except:
+            # 尚未存取則呼叫上面函式載入資料集，並儲存為.npy檔
             try:
                 if not os.path.exists('dataset_array/All_X_train.npy'):
                     X_train, Y_train = all('train')
@@ -109,7 +117,7 @@ def load_data(*args):
             except:
                 print('Can\'t download data')
     else:
-        ASCII = args[0]
+        ASCII = args[0] #載入單一字母資料集
         try:
             X_train = np.load('dataset_array/{0}_X_train.npy'.format(ASCII))
             Y_train = np.load('dataset_array/{0}_Y_train.npy'.format(ASCII))
@@ -139,6 +147,3 @@ def load_data(*args):
                 print('Can\'t download data')
 
     return X_train, Y_train, X_test, Y_test
-
-X_train, Y_train, X_test, Y_test = load_data(65)
-print()
